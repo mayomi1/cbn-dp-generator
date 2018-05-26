35030c2c34ff141200cb34c27d82d7f89c33860d
Ayandiran Mayomi <ayandiranmayomi@gmail.com>
2018-05-26 16:25:55 +0100

    Update renderImage.js

--- controllers/renderImage.js
+++ controllers/renderImage.js
@@ -17,7 +17,7 @@ class RenderImage {
         let test =  cloudinary.image( req.session.image+'.png',
 
             {
-                underlay: 'cbnnewerone_km5sk4',
+                underlay: 'cbnnewerone_orjox2',
                 //underlay: req.session.image+'.png',
                 width: 700, height: 700,
                 transformation: {crop: 'fill', height: 280, width: 280 , gravity: 'face', radius: 'max'}
